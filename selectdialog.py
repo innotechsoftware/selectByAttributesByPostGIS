01cc5ca2409fc64900b143d172b2709c8b153014
innotechsoftware <meyildirim@innotechyazilim.com>
2013-12-24 19:29:56 +0200

    Update selectdialog.py

--- selectdialog.py
+++ selectdialog.py
@@ -7,14 +7,14 @@
                              -------------------
         begin                : 2013-02-11
         copyright            : (C) 2013 by Innotech
-        email                : osman@gmail.com
+        email                : info@innotechyazilim.com
  ***************************************************************************/
 
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
